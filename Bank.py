9a78fc3d9b2fc3dee4d0b20678178b3a706db890
eannaforde <159945358+eannaforde@users.noreply.github.com>
2025-03-21 11:40:03 +0000

    First commit

---
+++ Bank.py
@@ -0,0 +1,10 @@
+#Bank.py
+#This program will prompt the user to read in two money amounts
+#Add the two amounts
+#Print the answer in a human readable format
+
+def main (): 
+#Ask user for the first amount in cents
+Amount1 = int(input("Enter amount one in cents"))
+#Ask user for the second amount in cents
+Amount2 = int(input("Enter amount two in cents"))
