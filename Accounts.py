578dc1ed5b3d63945e2f0c5ed3c9392de0c5f326
eannaforde <159945358+eannaforde@users.noreply.github.com>
2025-03-21 12:23:19 +0000

    Accounts.py initial commit

---
+++ Accounts.py
@@ -0,0 +1,9 @@
+#Accounts.py
+#This programme reads in a 10 character account number and outputs the account number with only the last 4 digits showing
+
+
+def main():
+
+#Ask the user to enter a 10 digit account number
+account_number = input("please enter a ten digit account number:")
+
