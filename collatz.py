35016cc732f8b4db712b1e142e38183f6c83af32
eannaforde <159945358+eannaforde@users.noreply.github.com>
2025-04-27 22:03:29 +0000

    Collatz.py update

--- collatz.py
+++ collatz.py
@@ -6,7 +6,21 @@
 #repeat until the number becomes 1 then end the program
 
 #ask the user to enter a number
+#input() is used to enter number as a string
+#int() converts string to an integer
 number = int(input("please enter a positive integer:"))
 
 #keep repeating until the number becomes 1
+#if checks if the nmber is even
+#else if the number is odd
 while number != 1:
+    if(number%2==0):
+        number=number//2
+    else:
+        number=number*3+1
+#program continues
+#sequence ends
+    print(number)
+#Reference:https://www.youtube.com/watch?v=lAp_5qTdOhM
+#Reference:https://stackoverflow.com/questions/33508034/making-a-collatz-program-automate-the-boring-stuff   
+#Reference:https://how.dev/answers/how-to-generate-the-collatz-sequence-in-python
