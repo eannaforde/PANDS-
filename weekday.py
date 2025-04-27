2641fb6c83310ae6c008472b7211c20187660342
eannaforde <159945358+eannaforde@users.noreply.github.com>
2025-04-27 18:56:56 +0000

    Comment update weekday.py

--- weekday.py
+++ weekday.py
@@ -5,12 +5,14 @@
 from datetime import datetime
 
 #This line will get todays day of the week as a number
-#Monday = 0, Tuesday = 1, ..., Saturday = 5, Sunday = 6
+#Monday = 0, Tuesday = 1, Saturday = 5, Sunday = 6 
 #Reference: https://docs.python.org/3/library/datetime.html#datetime.datetime.weekday
 
 today = datetime.today().weekday()
 
 #check if number is less than 5 (Monday to Friday)
+#use if else function to distinguish between weekday and weekend
+#Then se print function to return message as to what day it is
 if today < 5:
     #if it is then it is a weekday
     print("yes, unfortunately today is a weekday.")
@@ -22,3 +24,4 @@ else:
 #References
 #https://docs.python.org/3/library/datetime.html#datetime.datetime.weekday
 #https://stackoverflow.com/questions/9847213/how-do-i-get-the-day-of-week-given-a-date
+#https://labex.io/tutorials/python-how-to-write-an-efficient-python-function-to-check-if-a-date-is-a-weekday-398285
